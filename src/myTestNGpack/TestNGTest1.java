d1921823c50a808837bed8fec1ef8cb4544c9a1b
bjakkula <bjakkula@LIN80016387.corp.capgemini.com>
2019-02-25 13:00:06 +0530

    scripts updated

--- src/myTestNGpack/TestNGTest1.java
+++ src/myTestNGpack/TestNGTest1.java
@@ -1,7 +1,10 @@
 package myTestNGpack;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -10,18 +13,22 @@ import org.testng.annotations.Test;
 
 public class TestNGTest1 {
   @Test
-  public void Launch() 
+  public void Launch() throws Exception 
   {
 	  System.setProperty("webdriver.chrome.driver", "C:\\Seleniumhq\\chromedriver.exe");
 	  ChromeDriver driver=new ChromeDriver();
-	  driver.get("https://apsrtconline.in/oprs-web/login/show.do");
+	  driver.get("https://www.google.co.in");
 	  WebDriverWait w=new WebDriverWait(driver, 60);
-	  //w.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+	  w.until(ExpectedConditions.visibilityOfElementLocated(By.name("q"))).sendKeys("abdul kalam");//,Keys.ENTER
 	  driver.manage().window().maximize();
-	 
+	  
+	  WebElement e=driver.findElement(By.name("btnK"));
+	  Actions a=new Actions(driver);
+	  a.click(e).build().perform();
+	  /*
 	  String t=driver.getTitle();
 	  System.out.println(t);
-	  /*if(t.contains("Google")) 
+	  if(t.contains("Google")) 
 	  {
 		  Reporter.log("Title test Passed");
 		  Assert.assertTrue(true);	  
@@ -31,10 +38,8 @@ public class TestNGTest1 {
 	  {
 		  Reporter.log("Title test Failed");
 		  Assert.assertTrue(false);		  
-	  }
-	  
-	  driver.close();*/
-	  //System.out.println("i am one");
-
+	  }*/
+	  Thread.sleep(3000);
+	  driver.close();
   }
 }
